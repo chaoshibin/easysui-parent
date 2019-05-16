24c69885b310b9b984e49d978ae02cab2dd0b9b1
chaoshibin <shibin_chao@sui.com>
2019-05-16 12:21:44 +0800

    验证

---
+++ easysui-validate/src/main/java/com/easysui/validate/aspect/EasyValidateAspect.java
@@ -0,0 +1,47 @@
+package com.easysui.validate.aspect;
+
+import com.easysui.common.util.AspectUtil;
+import com.easysui.common.util.Result;
+import com.easysui.validate.annotation.EasyValidate;
+import com.easysui.validate.util.ValidateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+
+import java.util.Arrays;
+
+/**
+ * @author CHAO 2019/4/16
+ */
+@Slf4j
+@Aspect
+@Order(2)
+public class EasyValidateAspect {
+
+    @Pointcut("@annotation(com.easysui.validate.annotation.EasyValidate)")
+    public void pointCut() {
+
+    }
+
+    @Around("pointCut()")
+    public Object methodProcess(ProceedingJoinPoint joinPoint) throws Throwable {
+        Result<String> result = ValidateUtil.validate(Arrays.asList(joinPoint.getArgs()));
+        //验证失败
+        if (result.isError()) {
+            //返回类型
+            Class<?> returnType = AspectUtil.getReturnType(joinPoint);
+            EasyValidate easyValidateAnnotation = AspectUtil.getMethod(joinPoint).getAnnotation(EasyValidate.class);
+            //错误码域
+            String codeField = easyValidateAnnotation.codeField();
+            //错误信息域
+            String msgField = easyValidateAnnotation.msgField();
+            //验证失败错误码
+            String code = easyValidateAnnotation.code();
+            return AspectUtil.buildResult(returnType, codeField, msgField, code, result.getMsg());
+        }
+        return joinPoint.proceed();
+    }
+}

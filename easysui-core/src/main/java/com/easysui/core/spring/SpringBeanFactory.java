b1f666e065ffbaecdb8cf89180d4bef02c3f84ba
e <l>
2019-05-17 02:24:53 +0800

    jedis

--- easysui-core/src/main/java/com/easysui/core/spring/SpringBeanFactory.java
+++ easysui-core/src/main/java/com/easysui/core/spring/SpringBeanFactory.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Map;
 
 /**
- * @author chaoshibin
+ * @author CHAO
  */
 @Configuration
 public class SpringBeanFactory implements ApplicationContextAware {
